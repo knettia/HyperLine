2609b97116153e700f765faba29d19e021df9a7a
Amelia Gäßler <brunwault@gmail.com>
2025-01-15 09:42:02 +0200

    added parsing

--- include/hyperline/internal/html/__element.hpp
+++ include/hyperline/internal/html/__element.hpp
@@ -88,8 +88,11 @@ namespace hyperline::html::__1
 			__tag = std::move(t);
 		}
 
+		inline std::string get_tag() const
+		{ return __tag; }
+
 		template <typename __string_type, __hyperline_convertible_template(__string_type, std::string_view)>
-		inline bool has_attribute(const __string_type &k)
+		inline bool has_attribute(const __string_type &k) const
 		{
 			auto iter = __find_attribute(__attributes, k);
 
@@ -100,14 +103,14 @@ namespace hyperline::html::__1
 		}
 
 		template <typename __string_type, __hyperline_convertible_template(__string_type, std::string_view)>
-		inline attribute &get_attribute(const __string_type &k)
+		inline attribute &get_attribute(const __string_type &k) const
 		{
 			auto iter = __find_attribute(__attributes, k);
 
 			if (iter != __attributes.end())
 			{ return iter->value; }
 			
-			__throw_runtime_error(__hyperline_format("could not find attribute with key \"{0}\" in HTML element \"{1}\"", k, __tag));
+			__throw_runtime_error(__hyperline_format("could not find attribute with key \"{0}\" in HTML element <{1}>", k, __tag));
 		}
 
 		template <typename __string_type, __hyperline_convertible_template(__string_type, std::string_view)>
@@ -132,7 +135,7 @@ namespace hyperline::html::__1
 				return *this;
 			}
 
-			__throw_runtime_error(__hyperline_format("could not delete attribute with key \"{0}\" in HTML element \"{1}\"; no attribute found", k, __tag));
+			__throw_runtime_error(__hyperline_format("could not delete attribute with key \"{0}\" in HTML element <{1}>; no attribute found", k, __tag));
 		}
 
 		template <
@@ -194,7 +197,14 @@ namespace hyperline::html::__1
 
 			if (parse)
 			{
-
+				std::vector<std::variant<std::string, element>> content = element::parse(s);
+				for (std::variant<std::string, element> ic : content)
+				{
+					if (std::holds_alternative<std::string>(ic))
+					{ this->push_back(std::get<std::string>(ic)); }
+					else if (std::holds_alternative<element>(ic))
+					{ this->push_back(std::get<element>(ic)); }
+				}
 			}
 			else
 			{ __inner_content.push_back(std::string(s)); }
@@ -233,5 +243,122 @@ namespace hyperline::html::__1
 			os << represent(elem);
 			return os;
 		}
+
+		static std::vector<std::variant<std::string, element>> parse(const std::string &content)
+		{
+			std::vector<std::variant<std::string, element>> result;
+			std::vector<std::string> tag_stack;
+			size_t i = 0;
+
+			while (i < content.size())
+			{
+				while (i < content.size() && std::isspace(content[i])) ++i;
+
+				if (i < content.size() && content[i] == '<')
+				{
+					size_t tag_start = i;
+
+					size_t tag_end = content.find('>', tag_start);
+					if (tag_end == std::string::npos)
+					{ __throw_runtime_error("malformed tag; no closing '>' found"); }
+
+					std::string tag_content = content.substr(tag_start + 1, tag_end - tag_start - 1);
+
+					if (tag_content[0] == '/')
+					{
+						std::string closing_tag = tag_content.substr(1);
+						if (tag_stack.empty() || tag_stack.back() != closing_tag)
+						{ __throw_runtime_error(__hyperline_format("unexpected or mismatched closing tag </{0}>", closing_tag)); }
+
+						tag_stack.pop_back();
+						i = tag_end + 1;
+						break;
+					}
+
+					bool is_self_closing = tag_content.back() == '/';
+					if (is_self_closing)
+					{
+						tag_content.pop_back();
+					}
+
+					size_t space_pos = tag_content.find(' ');
+					std::string tag_name = tag_content.substr(0, space_pos);
+					element elem(tag_name);
+
+					if (space_pos != std::string::npos)
+					{
+						std::string attributes_str = tag_content.substr(space_pos + 1);
+						size_t attr_pos = 0;
+
+						while (attr_pos < attributes_str.size())
+						{
+							size_t key_end = attributes_str.find('=', attr_pos);
+							if (key_end == std::string::npos)
+							{ break; }
+
+							std::string key = attributes_str.substr(attr_pos, key_end - attr_pos);
+							key.erase(std::remove_if(key.begin(), key.end(), ::isspace), key.end());
+
+							size_t value_start = attributes_str.find('"', key_end + 1);
+							size_t value_end = attributes_str.find('"', value_start + 1);
+							if (value_start == std::string::npos || value_end == std::string::npos)
+							{ break; }
+
+							std::string value = attributes_str.substr(value_start + 1, value_end - value_start - 1);
+
+							elem.append_attribute(key, value);
+
+							attr_pos = value_end + 1;
+						}
+					}
+
+					i = tag_end + 1;
+
+					if (!is_self_closing)
+					{
+						tag_stack.push_back(tag_name);
+
+						std::vector<std::variant<std::string, element>> inner_content = parse(content.substr(i));
+
+						for (auto &child : inner_content)
+						{
+							if (std::holds_alternative<std::string>(child))
+							{ elem.push_back(std::get<std::string>(child)); }
+							else if (std::holds_alternative<element>(child))
+							{ elem.push_back(std::get<element>(child)); }
+						}
+
+						size_t closing_tag_start = content.find(__hyperline_format("</{0}>", tag_name), i);
+						if (closing_tag_start == std::string::npos)
+						{ __throw_runtime_error(__hyperline_format("malformed HTML: no closing tag after <{0}>", tag_name)); }
+
+						i = closing_tag_start + tag_name.size() + 3;
+						tag_stack.pop_back();
+					}
+
+					result.emplace_back(std::move(elem));
+				}
+				else
+				{
+					size_t text_end = content.find('<', i);
+					if (text_end == std::string::npos)
+					{
+						result.emplace_back(content.substr(i));
+						break;
+					}
+					else
+					{
+						result.emplace_back(content.substr(i, text_end - i));
+						i = text_end;
+					}
+				}
+			}
+
+			if (!tag_stack.empty())
+			{ __throw_runtime_error(__hyperline_format("unmatched opening tag <{0}>", tag_stack.back())); }
+
+			return result;
+		}
+
 	};
 }
